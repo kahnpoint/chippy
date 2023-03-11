3fcd4e94d72298686965cf708dbe22f3cd81fcd9
hardchaos <118440485+hardchaos@users.noreply.github.com>
2023-03-11 15:59:35 -0800

    added keyway support

---
+++ setup.py
@@ -0,0 +1,8 @@
+import keyway
+
+kw = keyway.Keyway()
+
+# enter your api keys and
+# delete this file after running it
+kw["DISCORD_TOKEN"] = "your discord bot token here"
+kw["OPENAI_KEY"] = "your openai api key here"
