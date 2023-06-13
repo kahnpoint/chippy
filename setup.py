bd8d3b20d3e6f291c971d12ab476991a47c4a6c0
mootline <mootline.com@gmail.com>
2023-06-13 12:29:09 -0700

    deleted setup.py (from keyway)

--- setup.py
+++
@@ -1,8 +0,0 @@
-import keyway
-
-kw = keyway.Keyway()
-
-# enter your api keys here
-# delete this file after running it
-kw["DISCORD_TOKEN"] = "your discord bot token here"
-kw["OPENAI_KEY"] = "your openai api key here"
